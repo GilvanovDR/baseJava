2081d26c28305f55b09b4778904f2e1a43e255ce
lucefer13+github@gmail.com <kulibin86>
2019-04-11 21:11:04 +0500

    HW3: Upd delete method on SortedArrayStorage

--- src/ru/GilvanovDr/WebApp/storage/SortedArrayStorage.java
+++ src/ru/GilvanovDr/WebApp/storage/SortedArrayStorage.java
@@ -2,25 +2,30 @@ package ru.GilvanovDr.WebApp.storage;
 
 import ru.GilvanovDr.WebApp.model.Resume;
 
-public class SortedArrayStorage extends AbstractArrayStorage{
+import java.util.Arrays;
+
+public class SortedArrayStorage extends AbstractArrayStorage {
     @Override
     protected int getIndex(String uuid) {
-        return 0;
-    }
-
-    @Override
-    public void delete(String uuid) {
-
+        return Arrays.binarySearch(storage, 0, size, new Resume(uuid));
     }
 
     @Override
     public void update(Resume resume) {
-
     }
 
     @Override
     public void save(Resume r) {
-
     }
 
+    @Override
+    public void delete(String uuid) {
+        final int index = getIndex(uuid);
+        if (index < 0) {
+            System.out.println("Resume " + uuid + " is not found in storage");
+        } else {
+            System.arraycopy(storage, index + 1, storage, index, size - index - 1);
+            size--;
+        }
+    }
 }
